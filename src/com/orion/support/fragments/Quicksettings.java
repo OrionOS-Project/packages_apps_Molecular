5978c76c76c330eb2292480d91abe0be8c87593e
Pranav Vashi <neobuddy89@gmail.com>
2024-05-17 23:57:09 +0900

    Molecular: Battery styles customizations [2/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>

--- src/com/orion/support/fragments/Quicksettings.java
+++ src/com/orion/support/fragments/Quicksettings.java
@@ -23,6 +23,8 @@ import android.content.pm.UserInfo;
 import android.os.Bundle;
 import android.os.UserHandle;
 import android.os.UserManager;
+import android.provider.Settings;
+
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceCategory;
@@ -33,6 +35,13 @@ import androidx.preference.SwitchPreference;
 
 import com.android.settings.R;
 import androidx.annotation.NonNull;
+
+import org.com.orion.support.preferences.SystemSettingListPreference;
+import com.orion.support.preferences.SystemSettingSwitchPreference;
+import com.orion.support.utils.DeviceUtils;
+
+import lineageos.preference.LineageSecureSettingSwitchPreference;
+import lineageos.providers.LineageSettings;
 
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.SettingsPreferenceFragment;
@@ -58,6 +67,16 @@ public class Quicksettings extends SettingsPreferenceFragment implements OnPrefe
     private LineageSecureSettingSwitchPreference mShowAutoBrightness;
     private SystemSettingSwitchPreference mBrightnessSliderHaptic;
 
+    private static final String KEY_BATTERY_STYLE = "qs_battery_style";
+    private static final String KEY_BATTERY_PERCENT = "qs_show_battery_percent";
+
+    private static final int BATTERY_STYLE_PORTRAIT = 0;
+    private static final int BATTERY_STYLE_TEXT = 4;
+    private static final int BATTERY_STYLE_HIDDEN = 5;
+
+    private SystemSettingListPreference mBatteryStyle;
+    private SystemSettingListPreference mBatteryPercent;
+
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
@@ -66,6 +85,17 @@ public class Quicksettings extends SettingsPreferenceFragment implements OnPrefe
         addPreferencesFromResource(R.xml.quicksettings_section);
         final ContentResolver resolver = getActivity().getContentResolver();
         final PreferenceScreen prefScreen = getPreferenceScreen();
+
+        mBatteryStyle = (SystemSettingListPreference) findPreference(KEY_BATTERY_STYLE);
+        mBatteryPercent = (SystemSettingListPreference) findPreference(KEY_BATTERY_PERCENT);
+
+        int batterystyle = Settings.System.getIntForUser(resolver,
+                Settings.System.QS_BATTERY_STYLE, BATTERY_STYLE_PORTRAIT, UserHandle.USER_CURRENT);
+
+        mBatteryStyle.setOnPreferenceChangeListener(this);
+
+        mBatteryPercent.setEnabled(
+                batterystyle != BATTERY_STYLE_TEXT && batterystyle != BATTERY_STYLE_HIDDEN);
 
         mShowBrightnessSlider = findPreference(KEY_SHOW_BRIGHTNESS_SLIDER);
         mShowBrightnessSlider.setOnPreferenceChangeListener(this);
@@ -104,6 +134,11 @@ public class Quicksettings extends SettingsPreferenceFragment implements OnPrefe
             mBrightnessSliderHaptic.setEnabled(value > 0);
             if (mShowAutoBrightness != null)
                 mShowAutoBrightness.setEnabled(value > 0);
+            return true;
+        } else if (preference == mBatteryStyle) {
+            int value = Integer.parseInt((String) newValue);
+            mBatteryPercent.setEnabled(
+                    value != BATTERY_STYLE_TEXT && value != BATTERY_STYLE_HIDDEN);
             return true;
         }
         return false;

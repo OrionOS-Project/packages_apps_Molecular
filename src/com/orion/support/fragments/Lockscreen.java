828a29aa1826805de457f7de12b8a5d2ad93c818
briant24 <briantadr24@gmail.com>
2025-01-28 00:54:25 +0700

    cleanup! Molecular: Clean up Unnecessary Log

--- src/com/orion/support/fragments/Lockscreen.java
+++ src/com/orion/support/fragments/Lockscreen.java
@@ -32,8 +32,6 @@ import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.Nullable;
 import com.orion.support.Molecular;
 
-import android.util.Log;
-
 import androidx.preference.Preference;
 import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceScreen;
@@ -104,11 +102,9 @@ public class Lockscreen extends SettingsPreferenceFragment implements OnPreferen
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        Log.d("molecular", "welcome locksreen");
         requireActivity().getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
             @Override
             public void handleOnBackPressed() {
-                Log.d("molecular", "back from lockscreen");
                 FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
                 fragmentManager.beginTransaction()
                         .replace(R.id.main_content, new Molecular())
--- src/com/orion/support/fragments/Quicksettings.java
+++ src/com/orion/support/fragments/Quicksettings.java
@@ -60,8 +60,6 @@ import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
 
-import android.util.Log;
-
 public class Quicksettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
     private static final String KEY_BATTERY_STYLE = "qs_battery_style";
@@ -305,11 +303,9 @@ public class Quicksettings extends SettingsPreferenceFragment implements OnPrefe
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        Log.d("molecular", "welcome qs");
         requireActivity().getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
             @Override
             public void handleOnBackPressed() {
-                Log.d("molecular", "back from qs");
                 FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
                 fragmentManager.beginTransaction()
                         .replace(R.id.main_content, new Molecular())

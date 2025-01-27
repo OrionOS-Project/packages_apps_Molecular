00dad2617e10742edf0aa1aae489bbd47bf725a8
briant24 <briantadr24@gmail.com>
2025-01-28 01:42:25 +0700

    Molecular: About us?
    Introducing the OrionOS Project and the members who worked hard on this project.
    The design is taken from several financial applications and the avatar reference is taken from Meta Emoji which can be taken via the WhatsApp by Meta application through the create avatar feature.
    
    Co-authored-by: Iverz <asterixiverz@gmail.com>
    Signed-off-by: briant24 <briantadr24@gmail.com>

--- src/com/orion/support/fragments/About.java
+++ src/com/orion/support/fragments/About.java
@@ -19,18 +19,13 @@ package com.orion.support.fragments;
 import android.content.Context;
 import android.content.ContentResolver;
 import android.content.Intent;
-import android.content.pm.UserInfo;
+import android.net.Uri;
 import android.os.Bundle;
-import android.os.UserHandle;
-import android.os.UserManager;
-import androidx.preference.Preference;
-import androidx.preference.PreferenceCategory;
-import androidx.preference.PreferenceScreen;
-import androidx.preference.Preference.OnPreferenceChangeListener;
-import android.provider.Settings;
 import com.android.settings.R;
 import androidx.annotation.NonNull;
 import android.util.Log;
+
+import androidx.cardview.widget.CardView;
 
 import android.view.View;
 import androidx.fragment.app.FragmentManager;
@@ -41,41 +36,42 @@ import com.orion.support.Molecular;
 
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.SettingsPreferenceFragment;
-import android.content.res.Resources;
-import java.util.Arrays;
-import java.util.ArrayList;
-import java.util.List;
 
-public class About extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+import com.android.settingslib.widget.LayoutPreference;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class About extends SettingsPreferenceFragment implements View.OnClickListener {
+
+    private LayoutPreference mLayoutPreference;
+    private Context mContext;
+    private Map<Integer, Uri> buttonUriMap;
+    private static final Uri GITHUB_URI_DATA = Uri.parse("https://github.com/OrionOS-Project");
+    private static final Uri WEBSITE_URI_DATA = Uri.parse("https://orionos.tech/");
+    private static final Uri TELEGRAM_URI_DATA = Uri.parse("https://t.me/OrionOS_prjkt");
+    private static final Uri FOUNDER_URI_DATA = Uri.parse("https://orionos.tech/team/hirokixd");
+    private static final Uri CO_FOUNDER_URI_DATA = Uri.parse("https://orionos.tech/team/romi.yusna");
+    private static final Uri CORE_DEV_URI_DATA = Uri.parse("https://orionos.tech/team/onle");
+    private static final Uri UI_UX_URI_DATA = Uri.parse("https://orionos.tech/team/iverz");
+    private static final Uri SUPPORTING_URI_DATA = Uri.parse("https://orionos.tech/team/smokey");
+    private static final Uri CONTRIBUTOR_URI_DATA = Uri.parse("https://orionos.tech/team/drenzzz");
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-        final Context context = getContext();
-        final Resources resources = context.getResources();
+        mContext = getContext();
         addPreferencesFromResource(R.xml.about_section);
-        final ContentResolver resolver = getActivity().getContentResolver();
-        final PreferenceScreen prefScreen = getPreferenceScreen();
-    }
-
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        return false;
-    }
-
-    @Override
-    public int getMetricsCategory() {
-        return MetricsProto.MetricsEvent.ORION;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        Log.d("molecular", "welcome about");
+        initializeButtonUriMap();
+        setupLayoutPreference();
         requireActivity().getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
             @Override
             public void handleOnBackPressed() {
-                Log.d("molecular", "back from about");
                 FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
                 fragmentManager.beginTransaction()
                         .replace(R.id.main_content, new Molecular())
@@ -85,4 +81,43 @@ public class About extends SettingsPreferenceFragment implements OnPreferenceCha
         });
     }
 
+    private void initializeButtonUriMap() {
+        buttonUriMap = new HashMap<>();
+        buttonUriMap.put(R.id.img_github, GITHUB_URI_DATA);
+        buttonUriMap.put(R.id.img_web, WEBSITE_URI_DATA);
+        buttonUriMap.put(R.id.img_tele, TELEGRAM_URI_DATA);
+        buttonUriMap.put(R.id.click_founder, FOUNDER_URI_DATA);
+        buttonUriMap.put(R.id.click_co_founder, CO_FOUNDER_URI_DATA);
+        buttonUriMap.put(R.id.click_core_dev, CORE_DEV_URI_DATA);
+        buttonUriMap.put(R.id.click_ui_ux, UI_UX_URI_DATA);
+        buttonUriMap.put(R.id.click_supp, SUPPORTING_URI_DATA);
+        buttonUriMap.put(R.id.click_cont, CONTRIBUTOR_URI_DATA);
+    }
+
+    private void setupLayoutPreference() {
+        mLayoutPreference = findPreference("about_us_section");
+        if (mLayoutPreference != null) {
+            View root = mLayoutPreference.findViewById(R.id.about_us_section);
+            for (int id : buttonUriMap.keySet()) {
+                CardView cardView = root.findViewById(id);
+                if (cardView != null) {
+                    cardView.setOnClickListener(this);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        Uri uri = buttonUriMap.get(view.getId());
+        if (uri != null && mContext != null) {
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            mContext.startActivity(intent);
+        }
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.ORION;
+    }
 }

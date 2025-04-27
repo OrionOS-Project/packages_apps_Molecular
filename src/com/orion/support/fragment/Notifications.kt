7a407129e1145f6caf53cf326893b08b437c66cb
NurKeinNeid <nurkeinneid@derpfest.org>
2025-04-27 20:17:06 +0200

    Molecular: Create notifications category
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>

---
+++ src/com/orion/support/fragment/Notifications.kt
@@ -0,0 +1,32 @@
+/*
+ * SPDX-FileCopyrightText: OrionOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.orion.support.fragment
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent
+
+import android.os.Bundle
+
+import androidx.preference.Preference
+
+import com.android.settings.R
+import com.android.settings.SettingsPreferenceFragment
+
+class Notifications : SettingsPreferenceFragment(), Preference.OnPreferenceChangeListener {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        addPreferencesFromResource(R.xml.notifications)
+    }
+
+    override fun onPreferenceChange(preference: Preference, newValue: Any?): Boolean {
+        return true
+    }
+
+    override fun getMetricsCategory(): Int = MetricsEvent.ORION
+
+    companion object {
+        const val TAG = "Molecular"
+    }
+}

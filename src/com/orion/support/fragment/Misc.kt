f4a6057f6f532b5ced942d957574d0af59879ff6
Wrdn28 <ifloraxd@gmail.com>
2025-08-10 15:29:00 +0200

    Molecular: Improve and reorganize PreferenceCategory structure
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>

--- src/com/orion/support/fragment/Misc.kt
+++ src/com/orion/support/fragment/Misc.kt
@@ -17,16 +17,31 @@ import com.android.settings.R
 import com.android.settings.SettingsPreferenceFragment
 
 class Misc : SettingsPreferenceFragment(), Preference.OnPreferenceChangeListener {
+    private var shouldRecreateOnExit: Boolean = false
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         addPreferencesFromResource(R.xml.misc)
+
+        // Recreate Settings when dashboard icon style changes, but only after leaving this page
+        findPreference<Preference>("theming_settings_dashboard_icons")?.onPreferenceChangeListener = this
     }
 
     override fun onPreferenceChange(preference: Preference, newValue: Any?): Boolean {
+        if (preference.key == "theming_settings_dashboard_icons") {
+            shouldRecreateOnExit = true
+        }
         return true
     }
 
     override fun getMetricsCategory(): Int = MetricsEvent.ORION
+
+    override fun onStop() {
+        super.onStop()
+        if (shouldRecreateOnExit && isAdded) {
+            requireActivity().recreate()
+            shouldRecreateOnExit = false
+        }
+    }
 
     companion object {
         const val TAG = "Molecular"

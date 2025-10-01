64a5f96270f7e9dfd7896e93b68cc73d811802ea
Abhay Singh Gill <abhaygill017@gmail.com>
2025-10-01 18:48:46 +0200

    Molecular: User selectable PIF [2/2]
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>

--- src/com/orion/support/fragment/Spoofing.java
+++ src/com/orion/support/fragment/Spoofing.java
@@ -28,6 +28,7 @@ import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 
 import com.orion.support.preference.KeyboxDataPreference;
+import com.orion.support.preference.PifDataPreference;
 
 import java.util.List;
 
@@ -38,9 +39,12 @@ public class Spoofing extends SettingsPreferenceFragment implements
     public static final String TAG = "Spoofing";
 
     private static final String KEYBOX_DATA_KEY = "keybox_data_setting";
+    private static final String PIF_DATA_KEY = "pif_data_setting";
 
     private ActivityResultLauncher<Intent> mKeyboxFilePickerLauncher;
+    private ActivityResultLauncher<Intent> mPifFilePickerLauncher;
     private KeyboxDataPreference mKeyboxDataPreference;
+    private PifDataPreference mPifDataPreference;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -63,6 +67,19 @@ public class Spoofing extends SettingsPreferenceFragment implements
                 }
             }
         );
+
+        mPifFilePickerLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
+                    Uri uri = result.getData().getData();
+                    Preference pref = findPreference(PIF_DATA_KEY);
+                    if (pref instanceof PifDataPreference) {
+                        ((PifDataPreference) pref).handleFileSelected(uri);
+                    }
+                }
+            }
+        );
     }
 
     @Override
@@ -74,8 +91,15 @@ public class Spoofing extends SettingsPreferenceFragment implements
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         mKeyboxDataPreference = findPreference(KEYBOX_DATA_KEY);
+        mPifDataPreference = findPreference(PIF_DATA_KEY);
+
         if (mKeyboxDataPreference != null) {
             mKeyboxDataPreference.setFilePickerLauncher(mKeyboxFilePickerLauncher);
+        }
+
+
+        if (mPifDataPreference != null) {
+            mPifDataPreference.setFilePickerLauncher(mPifFilePickerLauncher);
         }
     }
 

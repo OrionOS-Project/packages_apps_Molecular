e19fceeb55286160aba1b33fcbd50eaab425b209
Wrdn28 <ifloraxd@gmail.com>
2025-10-04 22:15:17 +0200

    Molecular: Implement notification row transparency settings
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>

--- src/com/orion/support/fragment/QS.kt
+++ src/com/orion/support/fragment/QS.kt
@@ -13,6 +13,8 @@ import android.os.Bundle
 import android.provider.Settings
 
 import androidx.preference.Preference
+import com.orion.support.preferences.SecureSettingSeekBarPreference
+import com.orion.support.preferences.SecureSettingSwitchPreference
 import androidx.preference.ListPreference
 
 import com.android.settings.R
@@ -22,22 +24,40 @@ class QS : SettingsPreferenceFragment(), Preference.OnPreferenceChangeListener {
 
     private lateinit var mDataUsagePreference: Preference
     private lateinit var mDataUsageCycleTypePreference: ListPreference
+    private lateinit var mNotifTransparencyLevelPref: SecureSettingSeekBarPreference
+    private lateinit var mNotifTransparencyContextPref: SecureSettingSwitchPreference
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         addPreferencesFromResource(R.xml.qs)
         
         mDataUsagePreference = findPreference("qs_show_data_usage")!!
         mDataUsageCycleTypePreference = findPreference("qs_data_usage_cycle_type")!!
+        mNotifTransparencyLevelPref = findPreference("notification_row_transparency_level")!!
+        mNotifTransparencyContextPref = findPreference("notification_row_transparency_context_aware")!!
         
         mDataUsageCycleTypePreference.setOnPreferenceChangeListener(this)
+        mNotifTransparencyLevelPref.setOnPreferenceChangeListener(this)
         
         updateDataUsageSummary()
+
+        // Initialize context-aware toggle availability based on current slider value
+        val currentLevel = Settings.Secure.getInt(
+            requireContext().contentResolver,
+            "notification_row_transparency_level",
+            15
+        )
+        updateTransparencyContextAvailability(currentLevel)
     }
 
     override fun onPreferenceChange(preference: Preference, newValue: Any?): Boolean {
         when (preference.key) {
             "qs_data_usage_cycle_type" -> {
                 updateDataUsageSummary(newValue as? String)
+                return true
+            }
+            "notification_row_transparency_level" -> {
+                val level = (newValue as? Int) ?: return false
+                updateTransparencyContextAvailability(level)
                 return true
             }
         }
@@ -58,6 +78,11 @@ class QS : SettingsPreferenceFragment(), Preference.OnPreferenceChangeListener {
         }
         
         mDataUsagePreference.summary = getString(summaryResId)
+    }
+
+    private fun updateTransparencyContextAvailability(level: Int) {
+        // Disable context-aware toggle when transparency is 0 (AOSP default opaque)
+        mNotifTransparencyContextPref.isEnabled = level > 0
     }
 
     override fun getMetricsCategory(): Int = MetricsEvent.ORION

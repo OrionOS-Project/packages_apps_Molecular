e9e4e880a7cdc5dd7facc2d3f5d1c4c2bc51fe7d
briantaudiera <briantaudieraa@gmail.com>
2025-11-10 16:36:34 +0000

    Molecular: Add Package Name

--- src/com/orion/support/compose/section/AboutSection.kt
+++ src/com/orion/support/compose/section/AboutSection.kt
@@ -21,7 +21,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import package com.orion.support.R
+import com.orion.support.R
 import androidx.compose.ui.res.stringResource
 
 @Composable
